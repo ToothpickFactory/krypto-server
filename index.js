fb324ad342648a7938eb9ed1c1e84b64f7fe4ff8
Lehi Thomas <lehithomas25@gmail.com>
2017-12-04 10:29:44 -0600

    added funcationality

--- index.js
+++ index.js
@@ -12,47 +12,8 @@ const apiURI = 'https://api.gdax.com';
 const sandboxURI = 'https://api-public.sandbox.gdax.com';
 
 const authedClient = new Gdax.AuthenticatedClient(key, b64secret, passphrase, sandboxURI);
-
-authedClient.getAccounts()
-	.then(data => {
-		console.log(data)
-	})
-	.catch(error => {
-		// handle the error
-	});
-
-authedClient.getOrders()
-	.then(data => {
-		console.log("Buy Order Data: " + data)
-	})
-	.catch(error => {
-		console.log("Error getting Orders ", error)
-	});
-
-	// Buy 1 BTC @ 100 USD
-// const buyParams = {
-// 	'price': '100.00', // USD
-// 	'size': '1',  // BTC
-// 	'product_id': 'BTC-USD',
-//   };
-//   authedClient.buy(buyParams)
-//   	.then(data => {
-// 		console.log("Buy Order Data: " + data.status)
-// 	})
-// 	.catch(error => {
-// 		// handle the error
-// 	});
-
-//const publicClient = new Gdax.PublicClient();
-
-// publicClient
-// 	.getProducts()
-// 	.then(data => {
-// 		console.log(data)
-// 	})
-// 	.catch(error => {
-// 		// handle the error
-// 	});
+const publicClient = new Gdax.PublicClient();
+const websocket = new Gdax.WebsocketClient(['ETH-USD']);
 
 cfonts.say('KRYPTO', {
 	font: 'block',
@@ -65,36 +26,91 @@ cfonts.say('KRYPTO', {
 	maxLength: '0'
   })
 
-// const websocket = new Gdax.WebsocketClient(['ETH-USD']);
 
-// const table = new Table({
-// 	chars: {
-// 	  'top': '-',
-// 	  'top-mid': '-',
-// 	  'top-left': '-',
-// 	  'top-right': '-',
-// 	  'bottom': '-',
-// 	  'bottom-mid': '-',
-// 	  'bottom-left': '-',
-// 	  'bottom-right': '-',
-// 	  'left': '║',
-// 	  'left-mid': '-' ,
-// 	  'mid': '-' ,
-// 	  'mid-mid': '-',
-// 	  'right': '║',
-// 	  'right-mid': '-',
-// 	  'middle': '│'
-// 	},
-// 	head: ['Price (USD)'].map(title => title.yellow),
-// 	colWidths: [14]
-//   })
+function getAccounts() {
+	authedClient.getAccounts()
+		.then(data => {
+			console.log(data)
+		})
+		.catch(error => {
+			// handle the error
+		}
+	);
+}
 
-// websocket.on('message', data => 
-// { 
-// 	if (data.type === "match") {
-// 		// console.log(table.toString())
-// 		// console.log(data.price)
-// 	}  
-// });
-// websocket.on('error', err => { console.log(err) });
-// websocket.on('close', () => { /* ... */ });
+function getOrders() {
+	authedClient.getOrders()
+		.then(data => {
+			console.log("Buy Order Data: " + data)
+		})
+		.catch(error => {
+			console.log("Error getting Orders ", error)
+		}
+	);
+}
+
+
+	// Buy 1 BTC @ 100 USD
+const buyParams = {
+	'price': '100.00', // USD
+	'size': '1',  // BTC
+	'product_id': 'BTC-USD',
+  };
+
+
+function placeBuyOrder(buyParams){
+	authedClient.buy(buyParams)
+		.then(data => {
+		console.log("Buy Order Data: " + data.status)
+		})
+		.catch(error => {
+			// handle the error
+		}
+	);
+}
+
+function getProducts(){
+	publicClient.getProducts()
+		.then(data => {
+			console.log(data)
+		})
+		.catch(error => {
+			// handle the error
+		}
+	);
+}
+
+
+const table = new Table({
+	chars: {
+	  'top': '-',
+	  'top-mid': '-',
+	  'top-left': '-',
+	  'top-right': '-',
+	  'bottom': '-',
+	  'bottom-mid': '-',
+	  'bottom-left': '-',
+	  'bottom-right': '-',
+	  'left': '║',
+	  'left-mid': '-' ,
+	  'mid': '-' ,
+	  'mid-mid': '-',
+	  'right': '║',
+	  'right-mid': '-',
+	  'middle': '│'
+	},
+	head: ['Price (USD)'].map(title => title.yellow),
+	colWidths: [14]
+  })
+
+function turnOnPriceStream(){
+	websocket.on('message', data => 
+	{ 
+		if (data.type === "match") {
+			console.log(table.toString())
+			console.log(data.price)
+		}  
+	});
+	websocket.on('error', err => { console.log(err) });
+	websocket.on('close', () => { /* ... */ });
+}
